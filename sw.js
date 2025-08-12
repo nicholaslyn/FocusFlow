94657401c5bedc33a7c43882326de99bf4088f11
nicholaslyn <nicholas.clyn@gmail.com>
2025-08-12 03:37:20 -0400

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,23 @@
+const CACHE = "focusflow-v1";
+const ASSETS = ["./", "./index.html", "./manifest.json", "./icon-192.png", "./icon-512.png"];
+
+self.addEventListener("install", (e) => {
+  e.waitUntil(caches.open(CACHE).then((c) => c.addAll(ASSETS)));
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", (e) => {
+  e.waitUntil(
+    caches.keys().then((keys) =>
+      Promise.all(keys.filter((k) => k !== CACHE).map((k) => caches.delete(k)))
+    )
+  );
+  self.clients.claim();
+});
+
+// Network-first; if offline, fall back to cache
+self.addEventListener("fetch", (e) => {
+  e.respondWith(
+    fetch(e.request).catch(() => caches.match(e.request)).then((r) => r || caches.match("./index.html"))
+  );
+});
